55bec2c92a9ecd198788071c26c39ca3d3364cdf
rifat-sarker <rifatswd@gmail.com>
2025-04-22 14:15:30 +0600

    Refactor createAdmin method to handle responses and errors properly

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
 import { userSerice } from "./user.service";
 
-
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userSerice.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userSerice.createAdmin(req.body);
+    res.status(201).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
