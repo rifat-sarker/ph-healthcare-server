bcd2367fa63b98f6643003ddfa6271bdb8c74e29
rifat-sarker <rifatswd@gmail.com>
2025-04-22 14:58:01 +0600

    Implementing Search/Filter Functionality Using searchTerm Query

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,13 +2,20 @@ import { Request, Response } from "express";
 import { adminSerivce } from "./admin.service";
 
 const getAllAdmin = async (req: Request, res: Response) => {
-  const result = await adminSerivce.getAllAdminFromDB();
-
-  res.status(200).json({
-    success: true,
-    message: "All admin data fetched",
-    data: result,
-  });
+  try {
+    const result = await adminSerivce.getAllAdminFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: "All admin data fetched",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error: error,
+    });
+  }
 };
 
 export const adminController = {
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,10 +1,28 @@
 import prisma from "../../utils/prisma";
 
-const getAllAdminFromDB = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdminFromDB = async (params: any) => {
+  console.log(params);
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
 export const adminSerivce = {
-    getAllAdminFromDB
-}
+  getAllAdminFromDB,
+};
