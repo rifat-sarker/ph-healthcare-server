d925a60702ccacf4f33220daf4f399d2a706d154
rifat-sarker <rifatswd@gmail.com>
2025-04-22 18:17:27 +0600

    Optimization of Search Code: Part 2

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,22 +3,16 @@ import prisma from "../../utils/prisma";
 
 const getAllAdminFromDB = async (params: any) => {
   const andConditions: Prisma.AdminWhereInput[] = [];
+
+  const adminSearchAbleFields = ["name", "email"];
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
